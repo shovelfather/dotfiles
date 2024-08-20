93ac1f7682536dfd9287f365310c0f90bd3b7b39
Shovelfather <shovelfather@gmail.com>
2024-08-19 23:31:25 -0400

    Update .config/nvim/lua/keymaps.lua Update .config/nvim/lua/plugins/neorg.lua

--- dot_config/nvim/lua/plugins/neorg.lua
+++ dot_config/nvim/lua/plugins/neorg.lua
@@ -3,39 +3,34 @@ return {
   build = ':Neorg sync-parsers',
   dependencies = { 'nvim-lua/plenary.nvim' },
   lazy = false,
-  config = function()
-    local opts = {
-      load = {
-        ['core.defaults'] = {},
-        ['core.concealer'] = {},
-        ['core.integrations.treesitter'] = {},
-        ['core.integrations.nvim-cmp'] = {},
-        ['core.clipboard'] = {},
-        ['core.clipboard.code-blocks'] = {},
-        ['core.text-objects'] = {},
-        ['core.esupports.metagen'] = {
-          config = {
-            author = 'shovelfather',
-            timezone = 'utc',
-            type = 'auto',
-          },
+  opts = {
+    load = {
+      ['core.defaults'] = {},
+      ['core.concealer'] = {},
+      ['core.integrations.treesitter'] = {},
+      ['core.integrations.nvim-cmp'] = {},
+      ['core.clipboard'] = {},
+      ['core.clipboard.code-blocks'] = {},
+      ['core.text-objects'] = {},
+      ['core.esupports.metagen'] = {
+        config = {
+          author = 'shovelfather',
+          timezone = 'utc',
+          type = 'auto',
         },
-        ['core.completion'] = {
-          config = {
-            engine = 'nvim-cmp',
-          },
+      },
+      ['core.completion'] = {
+        config = {
+          engine = 'nvim-cmp',
         },
-        ['core.dirman'] = {
-          config = {
-            workspaces = {
-              notes = '~/notes',
-            },
+      },
+      ['core.dirman'] = {
+        config = {
+          workspaces = {
+            notes = '~/notes',
           },
         },
       },
-    }
-    vim.keymap.set('n', '<k>', '<Plug>(neorg.text-objects.item-up)', {})
-    vim.keymap.set('n', '<j>', '<Plug>(neorg.text-objects.item-down)', {})
-    require('neorg').setup(opts)
-  end,
+    },
+  },
 }
