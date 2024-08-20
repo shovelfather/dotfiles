6cc16622a38f8f5a4c74247a2384ae97090c0614
Shovelfather <shovelfather@gmail.com>
2024-08-19 23:27:16 -0400

    Update .config/nvim/lua/plugins/neorg.lua

--- dot_config/nvim/lua/plugins/neorg.lua
+++ dot_config/nvim/lua/plugins/neorg.lua
@@ -3,39 +3,39 @@ return {
   build = ':Neorg sync-parsers',
   dependencies = { 'nvim-lua/plenary.nvim' },
   lazy = false,
-  opts = {
-    load = {
-      ['core.defaults'] = {},
-      ['core.concealer'] = {},
-      ['core.integrations.treesitter'] = {},
-      ['core.integrations.nvim-cmp'] = {},
-      ['core.clipboard'] = {},
-      ['core.clipboard.code-blocks'] = {},
-      ['core.text-objects'] = {},
-      ['core.esupports.metagen'] = {
-        config = {
-          author = 'shovelfather',
-          timezone = 'utc',
-          type = 'auto',
+  config = function()
+    local opts = {
+      load = {
+        ['core.defaults'] = {},
+        ['core.concealer'] = {},
+        ['core.integrations.treesitter'] = {},
+        ['core.integrations.nvim-cmp'] = {},
+        ['core.clipboard'] = {},
+        ['core.clipboard.code-blocks'] = {},
+        ['core.text-objects'] = {},
+        ['core.esupports.metagen'] = {
+          config = {
+            author = 'shovelfather',
+            timezone = 'utc',
+            type = 'auto',
+          },
         },
-      },
-      ['core.completion'] = {
-        config = {
-          engine = 'nvim-cmp',
+        ['core.completion'] = {
+          config = {
+            engine = 'nvim-cmp',
+          },
         },
-      },
-      ['core.dirman'] = {
-        config = {
-          workspaces = {
-            notes = '~/notes',
+        ['core.dirman'] = {
+          config = {
+            workspaces = {
+              notes = '~/notes',
+            },
           },
         },
       },
-    },
-  },
-  config = function()
+    }
     vim.keymap.set('n', '<k>', '<Plug>(neorg.text-objects.item-up)', {})
     vim.keymap.set('n', '<j>', '<Plug>(neorg.text-objects.item-down)', {})
-    require('neorg').setup()
+    require('neorg').setup(opts)
   end,
 }
