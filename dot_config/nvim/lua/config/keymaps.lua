8a74cb38e1c45d247a7518139168b1ecdb373d9b
Shovelfather <shovelfather@gmail.com>
2024-08-21 14:22:49 -0400

    Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/plugins/mini-surround.lua Update .config/nvim/lua/plugins/oil.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,48 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>fw", "<cmd>!firefox --new-window file:////%:p<CR>")
-vim.keymap.set("n", "d", '"_d')
-vim.keymap.set("v", "d", '"_d')
+vim.keymap.set('n', '<leader>fw', '<cmd>!firefox --new-window file:////%:p<CR>')
+vim.keymap.set('n', 'd', '"_d')
+vim.keymap.set('v', 'd', '"_d')
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- For oil.nvim file manager
+vim.keymap.set('n', '<Leader>fe', ':Oil<CR>', { desc = 'Open oil file manager' })
+
+-- Neorg specific, these move neorg items up and down.
+vim.keymap.set('n', '\\k', '<Plug>(neorg.text-objects.item-up)', {})
+vim.keymap.set('n', '\\j', '<Plug>(neorg.text-objects.item-down)', {})
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- vim: ts=2 sts=2 sw=2 et
