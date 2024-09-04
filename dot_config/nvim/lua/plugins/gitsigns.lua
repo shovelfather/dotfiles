9bc364ee5e414d9789509a838f5b5e4603e7d196
Shovelfather <shovelfather@gmail.com>
2024-09-04 19:08:49 -0400

    Update .config/nvim/lua/plugins/gitsigns.lua

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -6,7 +6,6 @@
 return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    event = 'LazyFile',
     opts = {
       signs = {
         add = { text = '+' },
