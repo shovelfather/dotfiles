0575c5be5639d7ce663d09fa31853008b0a4ece6
Shovelfather <shovelfather@gmail.com>
2024-09-12 14:29:47 -0400

    Update .config/nvim/lua/plugins/substitute.lua

--- dot_config/nvim/lua/plugins/substitute.lua
+++ dot_config/nvim/lua/plugins/substitute.lua
@@ -6,9 +6,9 @@ return {
     -- refer to the configuration section below
   },
   keys = {
-    { '<leader>e', require('substitute').operator, mode = 'n', noremap = true, desc = 'Substitute operator' },
-    { '<leader>ee', require('substitute').line, mode = 'n', noremap = true, desc = 'Substitute line' },
-    { '<leader>E', require('substitute').eol, mode = 'n', noremap = true, desc = 'Substitute eol' },
-    { '<leader>e', require('substitute').visual, mode = 'v', noremap = true, desc = 'Substitute visual' },
+    { '<leader>e', function() require('substitute').operator end, mode = 'n', noremap = true, desc = 'Substitute operator' },
+    -- { '<leader>ee', require('substitute').line, mode = 'n', noremap = true, desc = 'Substitute line' },
+    -- { '<leader>E', require('substitute').eol, mode = 'n', noremap = true, desc = 'Substitute eol' },
+    -- { '<leader>e', require('substitute').visual, mode = 'v', noremap = true, desc = 'Substitute visual' },
   },
 }
