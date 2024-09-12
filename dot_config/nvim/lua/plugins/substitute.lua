1761eb4194a4cf849c0d0a81f9d1cbb32b0ad807
Shovelfather <shovelfather@gmail.com>
2024-09-12 14:14:40 -0400

    Update .config/nvim/lua/plugins/substitute.lua

--- dot_config/nvim/lua/plugins/substitute.lua
+++ dot_config/nvim/lua/plugins/substitute.lua
@@ -1,14 +1,14 @@
 return {
-  "gbprod/substitute.nvim",
+  'gbprod/substitute.nvim',
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
   keys = {
-    { '<leader>e', 'require('substitute').operator', mode = 'n', noremap = true, desc = 'Substitute operator', },
-    { '<leader>ee', 'require('substitute').line', mode = 'n', noremap = true, desc = 'Substitute line', },
-    { '<leader>E', 'require('substitute').eol', mode = 'n', noremap = true, desc = 'Substitute eol', },
-    { '<leader>e', 'require('substitute').visual', mode = 'v', noremap = true, desc = 'Substitute visual', },
+    { '<leader>e', require('substitute').operator, mode = 'n', noremap = true, desc = 'Substitute operator' },
+    { '<leader>ee', require('substitute').line, mode = 'n', noremap = true, desc = 'Substitute line' },
+    { '<leader>E', require('substitute').eol, mode = 'n', noremap = true, desc = 'Substitute eol' },
+    { '<leader>e', require('substitute').visual, mode = 'v', noremap = true, desc = 'Substitute visual' },
   },
 }
